124b0b213bb07f2545ba6246d2ea522706019302
bluesterar-lab <bluesterar@gmail.com>
2026-03-17 11:34:27 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // 新增这一行：开启静态导出
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    unoptimized: true, // 你已经有了这个，非常好，静态导出必须配置此项
   },
 }
 
